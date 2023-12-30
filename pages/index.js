c964ac631e82301d45d35f3d2a4e874e690b5874
Michael Esteban <mickel13@gmail.com>
2023-12-30 12:16:16 +1000

    swap to tw-to-css package

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,13 @@ import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import atomOneDark from "react-syntax-highlighter/dist/cjs/styles/hljs/atom-one-dark";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { twi } from "tw-to-css";
 import { CopyButton } from "../components/CopyButton";
 import Footer from "../components/Footer";
 
 export default function Home() {
   const [textarea, setTextArea] = useState("");
   const [css, setCSS] = useState("");
-  const [loading, setLoading] = useState(false);
 
   function handleInputChange(event) {
     setTextArea(event.target.value);
@@ -19,21 +17,15 @@ export default function Home() {
 
   async function getCSS(e) {
     e.preventDefault();
-    setCSS("");
-    setLoading(true);
+    let styles = twi(textarea);
 
-    fetch("/api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(textarea),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setCSS(data.css);
-        setLoading(false);
-      });
+    // Add space after each colon
+    styles = styles.replace(/:/g, ": ");
+
+    // Add new line after each rule
+    styles = styles.replace(/;/g, ";\n");
+
+    setCSS(styles);
   }
 
   const placeholder = `Enter tailwind utilities or HTML/JSX with class names \ne.g. grid grid-cols-1 gap-y-10 OR <div class="grid grid-cols-1 gap-y-10">`;
@@ -59,13 +51,7 @@ export default function Home() {
             onChange={(e) => handleInputChange(e)}
             draggable="false"
           />
-          {loading ? (
-            <button className={styles.buttonload} disabled>
-              <FontAwesomeIcon icon={faSpinner} spin={true} /> Loading
-            </button>
-          ) : (
-            <input type="submit" value="Convert" className={styles.submit} />
-          )}
+          <input type="submit" value="Convert" className={styles.submit} />
         </form>
         {css && (
           <>
